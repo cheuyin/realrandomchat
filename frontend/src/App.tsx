fc16a0e8fa0c19855f73d5cbeadb034a027418b1
cheuyin <yinstanleycheung@gmail.com>
2023-12-31 09:41:06 -0800

    Add chakra ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Button } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world! How you doing?</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello world! How you doing?</h1>
+            <Button>Hello!</Button>
+        </div>
+    );
 }
 
 export default App;
